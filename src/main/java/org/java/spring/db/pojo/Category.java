95feccd2226b654299b95292b76abc40d743aa8d
patrictobola <tobola92@gmail.com>
2023-12-15 10:34:14 +0100

    done category entity & done db + tested db, need to test manytomany relationship

--- src/main/java/org/java/spring/db/pojo/Category.java
+++ src/main/java/org/java/spring/db/pojo/Category.java
@@ -1,5 +1,65 @@
 package org.java.spring.db.pojo;
 
-public class Category {
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PreRemove;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Photo> photos;
+
+    @PreRemove
+    private void removeCategoryFromPhotos() {
+    	for (Photo photo : photos) {
+    		photo.getCategories().remove(this);
+    	}
+    }
+    
+    
+    public Category() {
+    }
+
+    public Category(String name) {
+    	setName(name);
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Photo> getPhotos() {
+		return photos;
+	}
+
+	public void setPhotos(List<Photo> photos) {
+		this.photos = photos;
+	}
 }
