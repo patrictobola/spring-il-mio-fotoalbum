9a44f7e9a7c89f25f12126352a85b1598a23fd3a
patrictobola <tobola92@gmail.com>
2023-12-15 10:24:50 +0100

    done photo entity

--- src/main/java/org/java/spring/db/pojo/Photo.java
+++ src/main/java/org/java/spring/db/pojo/Photo.java
@@ -1,5 +1,98 @@
 package org.java.spring.db.pojo;
 
-public class Photo {
+import java.util.Arrays;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "photo")
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private boolean visible;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Category> categories;
+
+    public Photo() {
+    }
+
+    public Photo(String title, String description, String url, boolean visible, Category... categories) {
+        setTitle(title);
+        setDescription(description);
+        setUrl(url);
+        setVisible(visible);
+        setCategories(categories);;
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+	private void setCategories(Category... categories) {
+		setCategories(Arrays.asList(categories));
+	}
 }
